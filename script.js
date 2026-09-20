548d3dce578b054888eeede35b216531ca139e7a
floriafend-cyber <floriafend@gmail.com>
2026-09-20 18:49:23 -0500

    Remove getCryptoPrices function and interval

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-async function getCryptoPrices() {
-  const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd");
-  const data = await response.json();
-  
-  document.getElementById("btc-price").innerText = "BTC: $" + data.bitcoin.usd;
-  document.getElementById("eth-price").innerText = "ETH: $" + data.ethereum.usd;
-}
 
-// Refresh every 5 seconds
-setInterval(getCryptoPrices, 5000);
-getCryptoPrices();
